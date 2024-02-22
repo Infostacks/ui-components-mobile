dfa220d18627edfaca0fe50c3f907a7af08e8300
Husnain Asghar <117710947+HusnainButt12@users.noreply.github.com>
2024-02-22 19:28:00 +0500

    Update index.ts
    feat:ui-component change

--- Components/index.ts
+++ Components/index.ts
@@ -2,6 +2,7 @@ import CustomButton from './package/Button'
 import AppButton from './package/AppButton'
 import RadioButton from './package/Radio Button'
 import CountryCode from './package/Country Code'
+import Category from './package/Category'
 import * as Icons from './package/Icons'
 
-export { Icons, CustomButton, AppButton, RadioButton, CountryCode }
+export { Icons, CustomButton, AppButton, RadioButton, CountryCode, Category }
